af69f0df295e09683d2dc11704bbdc863b58ac22
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2020-11-22 23:28:54 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
     print("temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT)) 
     if input.temperature(TemperatureUnit.FAHRENHEIT) > 70:
-        light.set_pixel_color(5, light.rgb(255,0,0))
+        light.set_all(light.rgb(255,0,0))
     elif input.temperature(TemperatureUnit.FAHRENHEIT) < 40:
-        light.set_pixel_color(5, light.rgb(0,255,0))
+        light.set_all(light.rgb(0,255,0))
     else:
-        light.set_pixel_color(5, light.rgb(0,0,255))
+        light.set_all(light.rgb(0,0,255))
