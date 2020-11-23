3b6838a474732f40d236a0cb6dff109fb6f1258d
JustinV879 <72768893+JustinV879@users.noreply.github.com>
2020-11-22 23:11:08 -0500

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
 while (true) {
     console.log("temperature: " + input.temperature(TemperatureUnit.Fahrenheit))
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 40) {
+        light.setPixelColor(5, light.rgb(0, 255, 0))
+    } else {
+        light.setPixelColor(5, light.rgb(0, 0, 255))
+    }
+    
 }
-if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
-    light.setPixelColor(5, light.rgb(255, 0, 0))
-} else if (input.temperature(TemperatureUnit.Fahrenheit) < 40) {
-    light.setPixelColor(5, light.rgb(0, 255, 0))
-}
-
